df3b3d786ca10682f36d126834ee896514675ab3
DavidVictor <david.v.c.2013dd@gmail.com>
2017-11-28 15:51:46 -0300

    Atualização

---
+++ HTML5Application/public_html/footer/index.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <link type="text/css" rel="stylesheet" href="css/inicio.css" />
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+  <main>
+
+  </main>
+
+  <!-- FOOTER CONCLUIDO -->
+  <footer class="page-footer footer-todo" id="footer-todo">
+    <div class="container conteudo">
+      <div class="row center s12">
+        <a class="white-text" href="https://twitter.com/IFCE_"><img src="img/twitter.png" class="rede-sociais"></a>
+        <a class="white-text" href="https://www.youtube.com/user/ifcecoficialcomunica?feature=results_main"><img src="img/youtube.png" class="rede-sociais"></a>
+        <a class="white-text" href="https://www.facebook.com/Instituto-Federal-de-Educa%C3%A7%C3%A3o-Ci%C3%AAncia-e-Tecnologia-do-Cear%C3%A1-IFCE-471223182903300/"><img src="img/facebook.png" class="rede-sociais"></a>
+        <a class="white-text" href="#!"><img src="img/email.png" class="rede-sociais"></a>
+      </div>
+      <div class="row center rede-conteiner">
+        <a class="white-text links" href="faleconosco.php">Sobre nós</a>
+        <a class="white-text links" href="NossaEquipe.php">Equipe</a>
+        <a class="white-text links" href="TrabalheConosco.php">Trabalhe conosco</a>
+        <a class="white-text" href="Contatos.php">CONTATO</a>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright footer-copy">
+    <div class="container center" >
+      <ul>
+        <li><p class="white-text text-lighten-3 equipe">Núcleo de Desenvolvimento de Softwares - NDS</p></li>
+        <li><p class="white-text text-lighten-3 equipe">Coordenadoria de Tecnologia da Informação - CTI</p></li>
+        <li><p class="white-text text-lighten-3 equipe">IFCE Campus Maracanaú - 2017</p></li>
+      </ul>
+    </div>
+  </div>
+</footer>
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
